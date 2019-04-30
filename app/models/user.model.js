536d2fb06aa3f9f832c6fd7945c01cc04cbcc771
Randall Po <polawatpo@msn.com>
2019-04-30 23:43:10 +0700

    Finish 6_10

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -47,3 +47,17 @@ userSchema.methods.authenticate = function(password) {
 }
 
 mongoose.model('User', userSchema);
+
+userSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+    _this.findOne({userName: possibleUsername}, function(err, user){
+        if (!err) {
+            if (!user) callback(possibleUsername);
+            else return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+        }
+        else {
+            callback(null);
+        }
+    });
+}
--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,10 @@
 module.exports = {
     debug: true,
     mongoUri: "mongodb://localhost/twitter_clone",
-    sessionSecret: 'dev_secret_key'
+    sessionSecret: 'dev_secret_key',
+    facebook: {
+        clientID: '393353194728569',
+        clientSecret: '1dd7255191d36ecb5cdc5ca490099d1e',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
 };
